718f0f9dfbe24c3334c60d6dd650d5eb5cfb441c
rhiannon macken <rhiannon.macken@homtail.com>
2023-06-23 02:51:02 +0000

    Do Week 1

--- codespace_startup.sh
+++ codespace_startup.sh
@@ -10,8 +10,8 @@ python ../course/set1/tests.py
 # Before you run this file, change these to be your name and email address, 
 # then take the # off the value from just before the quote. 
 # I.e. make #" int just "
-git config --global user.email #"email@example.com"
-git config --global user.name #"Ben Doherty"
+git config --global user.email "rhiannon.macken@homtail.com"
+git config --global user.name "rhiannon macken"
 
 # once you've done this, run this file by copying this command to the terminal:
 # bash codespace_startup.sh 
